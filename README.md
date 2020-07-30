2421ec1b8fc4d3247618f2bd18c937a581a9a83c
Savio Lage <saviohenrique.lage@unifei.edu.br>
2020-07-30 12:20:27 -0300

    First

---
+++ README.md
@@ -0,0 +1,21 @@
+# SpeedAudio Whatsapp 
+
+
+Google chrome extension that speeds up audio playback on whatsapp web
+
+<p align="center">
+  <img src="screen1.png" width="400">
+</p>
+
+## Technologies
+
+* 🧠 Javascript
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,30 @@
+const interval = setInterval(() => {
+    const header = document.querySelector("._1QUKR");
+
+    if (header) {
+        clearInterval(interval);
+
+        var rateIndex = 0;
+        const rates = [1, 1.25, 1.5, 1.75, 2];
+
+        const button = document.createElement('button');
+        button.innerHTML = `${rates[rateIndex]}x`;
+        button.classList.add('TwoTimesButton');
+        header.appendChild(button);
+
+        button.addEventListener('click', () => {
+
+            rateIndex++;
+            if (rateIndex > 4)
+                rateIndex = 0;
+
+            button.innerHTML = `${rates[rateIndex]}x`;
+
+            const audios = document.querySelectorAll('audio');
+
+            audios.forEach(audio => {
+                audio.playbackRate = rates[rateIndex];
+            })
+        })
+    }
+}, 1000);
